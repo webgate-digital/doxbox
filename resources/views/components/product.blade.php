f11c6e7acf9994125c0b62af6bbfc665325c33cb
Jakub Kudlac <kudlac91@gmail.com>
2022-07-17 16:52:49 +0200

    Made remaining strings translatable.

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -9,5 +9,5 @@
     </h4>
     <p class="product-box--price">{{$item['retail_price_discounted_formatted']}} @if($item['retail_discount']) <span class="product-box--price-old">{{$item['retail_price_formatted']}}</span> @endif
     </p>
-    <a href="{{route(locale() . '.product.detail', [$item['category']['slug'], $item['slug']])}}" class="product-box--cta button button--primary">zobraziť</a>
+    <a href="{{route(locale() . '.product.detail', [$item['category']['slug'], $item['slug']])}}" class="product-box--cta button button--primary">{{$translations['product.show_cta']['text']}}</a>
 </div>
--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -6,12 +6,12 @@
             <div class="container">
                 <div class="flex flex-wrap items-center">
                     <div class="w-full lg:w-1/2">
-                        <p class="h2 leading-none">Využite jedinečný</p>
+                        <p class="h2 leading-none">{{$translations['slider.text_1']['text']}}</p>
                         <h1 class="h1 !text-[160px] leading-none text-secondary">
-                            <p>Výpredaj</p>
+                            <p>{{$translations['slider.sale']['text']}}</p>
                         </h1>
                         <a href="https://frontstore.theshop.sk"
-                           class="button button--primary button--inline">Prejsť do obchodu</a>
+                           class="button button--primary button--inline">{{$translations['slider.go_to_shop']['text']}}</a>
                     </div>
                 </div>
             </div>
@@ -40,21 +40,21 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-36 w-36 my-12 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                             </svg>
-                            <h3 class="h3">Výhoda 1</h3>
+                            <h3 class="h3">{{$translations['advantages.item_title']['text']}} 1</h3>
                             <p>Sed at turpis maximus, mattis mauris et, sagittis dui. Nullam lobortis enim sapien, pretium elementum libero ornare id. Donec elit leo, placerat vitae ligula quis, pulvinar blandit elit. In elementum.</p>
                         </div>
                         <div class="w-full lg:w-1/3 lg:px-8">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-36 w-36 my-12 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                             </svg>
-                            <h3 class="h3">Výhoda 2</h3>
+                            <h3 class="h3">{{$translations['advantages.item_title']['text']}} 2</h3>
                             <p>Morbi fermentum dolor ac cursus dignissim. Aenean et erat ac metus ornare malesuada ac in diam. Suspendisse lacinia ultricies velit, vitae luctus sapien efficitur vitae. Curabitur a euismod dui, sed.</p>
                         </div>
                         <div class="w-full lg:w-1/3 lg:px-8">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-36 w-36 my-12 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
                             </svg>
-                            <h3 class="h3">Výhoda 3</h3>
+                            <h3 class="h3">{{$translations['advantages.item_title']['text']}} 3</h3>
                             <p>Nam ornare rhoncus imperdiet. Mauris semper sapien vitae neque tincidunt interdum. Sed volutpat faucibus ornare. Etiam id mattis purus. Suspendisse tempus risus vel leo suscipit, vitae cursus erat lobortis. Mauris.</p>
                         </div>
                     </div>
