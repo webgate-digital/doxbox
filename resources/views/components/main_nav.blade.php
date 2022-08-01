bb1c6e6e4907dd20405594d0c67b2bb600d7b7ad
Lukáš Minár <minar.lukas@gmail.com>
2022-08-01 11:56:40 +0000

    tweak navigation

--- resources/views/components/main_nav.blade.php
+++ resources/views/components/main_nav.blade.php
@@ -1,18 +1,7 @@
-<li>
-    <a href="{{route(locale() . '.product.list')}}">{{$translations['menu.products']['text']}}</a>
-</li>
 @foreach($headerPages as $headerPage)
     <li>
-        <a href="{{route(locale() . '.page', [$headerPage['slug']])}}">{{$headerPage['title']}}
+        <a href="{{route(locale() . '.page', [$headerPage['slug']])}}" class="{{$headerPage['slug'] == $page->slug ? 'active' : ''}}">
+            {{$headerPage['title']}}
         </a>
     </li>
 @endforeach
-{{--                            <li>--}}
-{{--                                <a href="{{route(locale() . '.blog.list')}}">{{$translations['menu.blog']['text']}}</a>--}}
-{{--                            </li>--}}
-<li>
-    <a href="{{route(locale() . '.blog.list')}}">{{$translations['menu.blog']['text']}}</a>
-</li>
-<li>
-    <a href="{{route(locale() . '.contact')}}">{{$translations['menu.contact']['text']}}</a>
-</li>
