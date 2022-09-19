ef78373ac5b2ec2adcb95c3d788b6d678d1ef4c8
Lukáš Minár <minar.lukas@gmail.com>
2022-09-19 13:59:30 +0000

    sync

---
+++ resources/js/vue/components/VNavigation.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="main-nav">
+        <div class="container">
+            <ul class="main-nav--menu">
+                <li v-for="item in rootItems" :key="item.uuid">
+                    <a :href="item.url">
+                        {{ item.name }}
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'VNavigation',
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
+    },
+    data: () => {
+        return {
+            active: false
+        }
+    },
+    computed: {
+        rootItems() {
+            return this.items.filter(item => !item.has_parent)
+        }
+    },
+    mounted() {
+        console.log(this.items)
+    }
+}
+</script>
