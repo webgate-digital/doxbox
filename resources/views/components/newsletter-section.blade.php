46d0e824a7eaf33fdcbbf2503ac4439f0ee567e8
Lukáš Minár <minar.lukas@gmail.com>
2022-11-09 09:19:41 +0000

    tweak newsletter button

--- resources/views/components/newsletter-section.blade.php
+++ resources/views/components/newsletter-section.blade.php
@@ -17,7 +17,7 @@
                 @csrf
                 <div class="md:flex">
                     <input id="email" type="email" class="input rounded-lg p-4 disabled:opacity-50 disabled:cursor-not-allowed" placeholder="Vyplň e-mail" required name="email">
-                    <button class="button button--primary !inline-flex items-center !w-auto md:ml-6 mt-4 md:mt-0 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed" type="submit">
+                    <button class="w-full md:w-auto justify-center button button--primary !inline-flex items-center md:ml-6 mt-4 md:mt-0 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed" type="submit">
                         <svg class="animate-spin hidden mr-3 h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
