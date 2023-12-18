59403921d6b8f5a1bc7a98ffb63d13af08683530
Lukáš Minár <minar.lukas@gmail.com>
2023-12-18 09:49:22 +0000

    add overlay on products filter

--- resources/views/components/product_sidebar.blade.php
+++ resources/views/components/product_sidebar.blade.php
@@ -15,11 +15,18 @@
                         <div>{{ $attribute['name'] }}</div>
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M20.0801 6.83003C20.1739 6.73537 20.3017 6.68213 20.4351 6.68213C20.5684 6.68213 20.6962 6.73537 20.7901 6.83003L21.8501 7.89003C21.9467 7.98076 22.0016 8.10744 22.0016 8.24003C22.0016 8.37261 21.9467 8.49929 21.8501 8.59003L12.6601 17.78C12.5195 17.9207 12.3289 17.9999 12.1301 18H11.8701C11.6712 17.9999 11.4806 17.9207 11.3401 17.78L2.15005 8.59003C2.05338 8.49929 1.99854 8.37261 1.99854 8.24003C1.99854 8.10744 2.05338 7.98076 2.15005 7.89003L3.21005 6.83003C3.30394 6.73537 3.43174 6.68213 3.56505 6.68213C3.69837 6.68213 3.82617 6.73537 3.92005 6.83003L12.0001 14.91L20.0801 6.83003Z" fill="#131416"/>
-                        </svg>                            
+                        </svg>
                     </div>
+                    <script>
+                        const onAttributeClick = (event) => {
+                            const $list = document.querySelector('#list');
+                            $list?.classList.add('loading');
+                            document.getElementById('filter-form').submit();
+                        }
+                    </script>
                     <div class="attribute-block__values flex flex-col">
                         @foreach ($attribute['values'] as $attributeValue)
-                            <div onclick="document.getElementById('filter-form').submit();"
+                            <div onclick="onAttributeClick(event)"
                                 class="mb-4 flex items-center gap-4 cursor-pointer filter-bar--attribute">
                                 <input class="cursor-pointer" id="attribute_{{ $attributeValue['uuid'] }}"
                                     name="attributes[{{ $attribute['uuid'] }}][]" type="checkbox"
