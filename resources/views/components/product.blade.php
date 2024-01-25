6c3ce6cc73d1770e0d938a79543326e7d739d27b
Lukáš Minár <minar.lukas@gmail.com>
2024-01-25 13:48:12 +0000

    add badges

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,4 +1,16 @@
-
+@php
+    // Calculate discount percentage for V3
+    $isV3 = isset($item['_highlightResult']);
+    if($isV3) {
+        if ($salePrice != $retailPrice && $retailPrice > 0) {
+            $discountPercentage = round(100 - ($salePrice / $retailPrice * 100));
+        }
+    } else {
+        if ($item['retail_price'] != $item['retail_price_discounted'] && $item['retail_price'] > 0) {
+            $discountPercentage = round(100 - ($item['retail_price_discounted'] / $item['retail_price'] * 100));
+        }
+    }
+@endphp
 <a href="{{\App\Http\Controllers\ProductController::buildProductRoute($item)}}" class="product-box{{isset($item['is_sold_out']) && $item['is_sold_out'] ? ' product-box--sold-out' : ''}}">
     <div class="product-box--image">
         <img src="{{$item['image_url']}}" alt="{{$item['name']}}">
@@ -8,6 +20,11 @@
         @if(isset($item['badge']) && $item['badge'])
             <div class="product-box--badge" style="--badge-font-color: {{$item['badge']['font_color']}}; --badge-background-color: {{$item['badge']['background_color']}};">
                 {{$item['badge']['name']}}
+            </div>
+        @endif
+        @if(isset($discountPercentage))
+            <div class="product-box--badge product-box--badge-discount">
+                - {{$discountPercentage}}%
             </div>
         @endif
         @if(isset($item['is_sold_out']) && $item['is_sold_out'])
